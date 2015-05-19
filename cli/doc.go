6228d2e01eb5d7092cbc19fd61ce9ef84478edc0
Wayne See <weynsee@gmail.com>
2015-05-19 15:48:37 +0800

    add a doc.go

---
+++ cli/doc.go
@@ -0,0 +1,14 @@
+/*
+Package cli allows the user to create a PhraseApp cli app.
+
+The cli app has 4 commands:
+
+    init    Initializes a phrase project
+    pull    Download the translation files in the current project
+    push    Upload the translation files in the current project to PhraseApp
+    tags    List all the tags in the current project
+
+The cli implements all the commands and subcommands implemented by the
+official PhraseApp command-line client.
+*/
+package cli
